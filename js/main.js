1ea17beadf6470cfab1c31801b1d94fce62226de
Bernardo Baquero Stand <baquerober@gmail.com>
2014-03-01 23:38:55 +0100

    Updated screenshots and made them pressable to slide them in

--- js/main.js
+++ js/main.js
@@ -15,6 +15,34 @@ ajax({
 	}
 });
 
+
+var screen = {
+	container: document.querySelector("#screens .img"),
+	current: 2,
+	max: 3,
+	switch: function() {
+		var next = screen.current + 1;
+		if (next > screen.max || (next >= 3 && window.innerWidth < 960)) {
+			next = 1;
+		}
+
+		screen.container.classList.remove("slide-in");
+		// Reset Animations
+		screen.container.offsetWidth = screen.container.offsetWidth;
+		// Switch Screens
+		screen.container.classList.remove("screen" + screen.current);
+		screen.container.classList.add("screen" + next);
+		screen.current = next;
+		screen.slideIn();
+	},
+	slideIn: function() {
+		screen.container.classList.add("slide-in");
+	}
+};
+
 function loadAnim() {
-	document.querySelector("#screens .img").classList.add("slide-in");
+	screen.slideIn();
+	screen.container.addEventListener("click", function() {
+		screen.switch();
+	});
 }
